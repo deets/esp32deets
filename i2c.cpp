91fb1e3a731b6d82fbb618c1d6415b581434243c
Diez B. Roggisch <deets@web.de>
2020-08-02 13:09:36 +0200

    Introduce reading into buffer given by pointer & size

--- i2c.cpp
+++ i2c.cpp
@@ -85,3 +85,26 @@ void I2CHost::write_byte_to_register(uint8_t address, uint8_t register_, uint8_t
   err = i2c_master_cmd_begin(_i2c_num, cmd, 1000 / portTICK_RATE_MS);
   assert(err == ESP_OK);
 }
+
+void I2CHost::read_from_device_register_into_buffer(uint8_t address, uint8_t register_, uint8_t* buffer, size_t length) const
+{
+  esp_err_t err;
+  // send the request
+  i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+  i2c_master_start(cmd);
+  i2c_master_write_byte(cmd, (address << 1) | I2C_MASTER_WRITE, 1);
+  i2c_master_write_byte(cmd,  register_, 1);
+  i2c_master_stop(cmd);
+  err = i2c_master_cmd_begin(_i2c_num, cmd, 1000 / portTICK_RATE_MS);
+  print_error(err);
+  assert(err == ESP_OK);
+  i2c_cmd_link_delete(cmd);
+  cmd = i2c_cmd_link_create();
+  i2c_master_start(cmd);
+  i2c_master_write_byte(cmd, (address << 1) | I2C_MASTER_READ, 1);
+  i2c_master_read(cmd, buffer, length, I2C_MASTER_LAST_NACK);
+  i2c_master_stop(cmd);
+  err = i2c_master_cmd_begin(_i2c_num, cmd, 1000 / portTICK_RATE_MS);
+  assert(err == ESP_OK);
+  i2c_cmd_link_delete(cmd);
+}
