c67a59732532cb518cb66dabb5e871d705c079a8
Diez B. Roggisch <deets@web.de>
2021-07-11 12:54:47 +0200

    Rework to SDK 4.2, especially the common event loop

--- wifi.cpp
+++ wifi.cpp
@@ -28,46 +28,44 @@ static EventGroupHandle_t s_wifi_event_group;
  * - are we connected to the AP with an IP? */
 const int WIFI_CONNECTED_BIT = 1;
 
-int s_retry_num = 0;
-
-esp_err_t event_handler(void *ctx, system_event_t *event)
+void event_handler(void* arg, esp_event_base_t event_base,
+		   int32_t event_id, void* event_data)
 {
-    switch(event->event_id) {
-    case SYSTEM_EVENT_STA_START:
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+      ESP_LOGE(TAG, "WIFI_EVENT, WIFI_EVENT_STA_START");
         esp_wifi_connect();
-        break;
-    case SYSTEM_EVENT_STA_GOT_IP:
-      //ESP_LOGI(TAG, "got ip: %s", ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
-        s_retry_num = 0;
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+      ESP_LOGE(TAG, "WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED");
+      esp_wifi_connect();
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
         xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
-        break;
-    case SYSTEM_EVENT_STA_DISCONNECTED:
-        {
-            if (s_retry_num < CONFIG_DEETS_WIFI_MAXIMUM_RETRY) {
-                esp_wifi_connect();
-                xEventGroupClearBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
-                s_retry_num++;
-                ESP_LOGI(TAG,"retry to connect to the AP");
-            }
-            ESP_LOGI(TAG,"connect to the AP fail\n");
-            break;
-        }
-    default:
-        break;
     }
-    return ERR_OK;
 }
+
+
 
 void wifi_init_sta(const std::string& ssid, const std::string& password)
 {
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &event_handler,
+                                                        NULL,
+                                                        NULL));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &event_handler,
+                                                        NULL,
+                                                        NULL));
+
     s_wifi_event_group = xEventGroupCreate();
 
-    tcpip_adapter_init();
-    esp_err_t err = esp_event_loop_init(event_handler, NULL);
-    ESP_ERROR_CHECK(err);
-
+    esp_netif_init();
+    esp_netif_create_default_wifi_sta();
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
     wifi_config_t wifi_config = {};
     strncpy(reinterpret_cast<char*>(&wifi_config.sta.ssid[0]), ssid.c_str(), sizeof(wifi_config.sta.ssid));
     strncpy(reinterpret_cast<char*>(&wifi_config.sta.password[0]), password.c_str(), sizeof(wifi_config.sta.password));
@@ -79,6 +77,7 @@ void wifi_init_sta(const std::string& ssid, const std::string& password)
     ESP_LOGI(TAG, "wifi_init_sta finished.");
     ESP_LOGI(TAG, "connect to ap SSID:%s password:%s",
              ssid.c_str(), password.c_str());
+
 }
 
 struct network_entry_t
