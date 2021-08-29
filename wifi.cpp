e9194778eb2628700a8c0714ee109ea03f9f9dae
Diez B. Roggisch <deets@web.de>
2021-08-29 14:31:34 +0200

    Update connected state in case we lost the configuration

--- wifi.cpp
+++ wifi.cpp
@@ -43,10 +43,22 @@ void event_handler(void* arg, esp_event_base_t event_base,
     } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
       ESP_LOGE(TAG, "WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED");
       esp_wifi_connect();
-    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
-        ESP_LOGI(TAG, "IP_EVENT, IP_EVENT_STA_GOT_IP, got ip:" IPSTR, IP2STR(&event->ip_info.ip));
-        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    } else if (event_base == IP_EVENT)
+    {
+      switch(event_id)
+      {
+      case IP_EVENT_STA_GOT_IP:
+	{
+	  ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+	  ESP_LOGI(TAG, "IP_EVENT, IP_EVENT_STA_GOT_IP, got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+	  xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+	}
+	break;
+      case IP_EVENT_STA_LOST_IP:
+	ESP_LOGI(TAG, "IP_EVENT, IP_EVENT_STA_LOST_IP");
+	xEventGroupClearBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+	break;
+      }
     }
 }
 
