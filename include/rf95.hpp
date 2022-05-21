f21621258ed8722f879cc2d5cdd3fd0384d197ed
Diez B. Roggisch <deets@web.de>
2022-05-21 11:06:17 +0200

    We can hard-coded send a few bytes

--- include/rf95.hpp
+++ include/rf95.hpp
@@ -4,20 +4,64 @@
 #include <driver/spi_master.h>
 #include <driver/gpio.h>
 
+#include <sched.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <array>
 
 class RF95
 {
 public:
+  enum mode_t {
+    IDLE,
+    TX
+  };
+
+  enum class register_t : uint8_t
+  {
+    FIFO = 0x00,
+    PAYLOAD_LENGTH = 0x22,
+    OP_MODE = 0x01,
+    FREQ_MSB = 0x06,
+    FREQ_MID = 0x07,
+    FREQ_LSB = 0x08,
+    PA_CONFIG = 0x09,
+    PA_DAC = 0x4D,
+    FIFO_ADDR_PTR = 0xD,
+    FIFO_TX_BASE = 0x0E,
+    FIFO_RX_BASE = 0x0F,
+    MODEM_CONFIG_1 = 0x1D,
+    MODEM_CONFIG_2 = 0x1E,
+    MODEM_CONFIG_3 = 0x26,
+    PREAMBLE_MSB = 0x20,
+    PREAMBLE_LSB = 0x21,
+  };
+
+  enum class op_reg : uint8_t
+  {
+    LORA = 1 << 7,
+    SLEEP = 0b000,
+    STDBY = 0b001,
+    FSTX = 0b010,
+    TX = 0b011,
+    FSRX = 0b100,
+    RX = 0b101,
+  };
+
   RF95(spi_host_device_t spi_host, gpio_num_t cs, gpio_num_t sck, gpio_num_t mosi, gpio_num_t miso, int speed);
   ~RF95();
 
-  uint8_t reg_read(uint8_t register_);
-  void reg_write(uint8_t register_, uint8_t value);
-
+  uint8_t reg_read(register_t register_);
+  void reg_write(register_t register_, uint8_t value);
+  void send(const uint8_t* buffer, size_t len);
 private:
-  //uint8_t reg_write(uint8_t reg, uint8_t value);
+  void mode(mode_t mode);
+  void setup();
+  void modem_config(const std::array<uint8_t, 3>& config);
+  void preamble_length(uint16_t);
+  void frequency(float);
+  void tx_power(int);
+
 
   spi_host_device_t _spi_host;
   spi_device_handle_t _spi;
--- rf95.cpp
+++ rf95.cpp
@@ -1,11 +1,26 @@
 #include "rf95.hpp"
 #include "driver/spi_common.h"
 
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 #include <esp_log.h>
 
 #include <cstring>
+#include <initializer_list>
+#include <sys/types.h>
 
 #define TAG "rf95"
+
+
+#define RH_RF95_MAX_POWER 0x70
+#define RH_RF95_PA_DAC_DISABLE 0x04
+#define RH_RF95_PA_DAC_ENABLE  0x07
+#define RH_RF95_PA_SELECT 0x80
+
+// The crystal oscillator frequency of the module
+#define RH_RF95_FXOSC 32000000.0
+// The Frequency Synthesizer step = RH_RF95_FXOSC / 2^^19
+#define RH_RF95_FSTEP  (RH_RF95_FXOSC / 524288)
 
 #define SPI_ERROR_CHECK \
   switch(spi_res) \
@@ -22,6 +37,14 @@
     assert(false);                              \
   }
 
+namespace {
+
+uint8_t operator | (const RF95::op_reg& left, const RF95::op_reg& right)
+{
+  return uint8_t(int(left) | int(right));
+}
+
+}
 
 RF95::RF95(spi_host_device_t spi_host, gpio_num_t cs, gpio_num_t sck,
            gpio_num_t mosi, gpio_num_t miso, int speed)
@@ -41,6 +64,10 @@ RF95::RF95(spi_host_device_t spi_host, gpio_num_t cs, gpio_num_t sck,
     sck, // sclk_io_num
     -1, // quadwp_io_num
     -1, // quadhd_io_num
+    -1, // data4_io_num
+    -1, // data5_io_num
+    -1, // data6_io_num
+    -1, // data7_io_num
     0, // max_transfer_sz
     0, // flags
     0 // intr_flags
@@ -68,18 +95,129 @@ RF95::RF95(spi_host_device_t spi_host, gpio_num_t cs, gpio_num_t sck,
   SPI_ERROR_CHECK;
   spi_res = spi_bus_add_device(_spi_host, &devcfg, &_spi);
   SPI_ERROR_CHECK;
+
+  setup();
 }
+
 
 RF95::~RF95() {}
 
-uint8_t RF95::reg_read(uint8_t register_)
+
+void RF95::setup()
+{
+  // This is based on the RadioHead library
+  // Sleep, and set LoRa operation mode
+  const auto desired_mode = op_reg::LORA | op_reg::SLEEP;
+  reg_write(register_t::OP_MODE, desired_mode);
+  vTaskDelay(10 / portTICK_PERIOD_MS);
+  assert(desired_mode == reg_read(register_t::OP_MODE));
+
+  // set up FIFO
+  // we configure so that we can use the entire 256 byte FIFO for either receive
+  // or transmit, but not both at the same time
+  for(const auto reg : { register_t::FIFO_TX_BASE, register_t::FIFO_RX_BASE})
+  {
+    reg_write(reg, 0);
+  }
+  mode(IDLE);
+
+  modem_config({ 0x72, 0x74, 0x04});
+  preamble_length(8);
+  frequency(868.0);
+  tx_power(13);
+}
+
+
+void RF95::send(const uint8_t *buffer, size_t len) {
+  mode(IDLE);
+  // if (!waitCAD())
+  //   return false;  // Check channel activity
+
+  // Position at the beginning of the FIFO
+  reg_write(register_t::FIFO_ADDR_PTR, 0);
+  // The headers
+  reg_write(register_t::FIFO, 0xAA);
+  reg_write(register_t::FIFO, 0x55);
+  reg_write(register_t::FIFO, 0xFF);
+  reg_write(register_t::FIFO, 0x33);
+  // The message data
+  //spiBurstWrite(RH_RF95_REG_00_FIFO, data, len);
+
+  reg_write(register_t::PAYLOAD_LENGTH, 4);
+  mode(TX);
+}
+
+
+void RF95::modem_config(const std::array<uint8_t, 3>& config)
+{
+  reg_write(register_t::MODEM_CONFIG_1, config[0]);
+  reg_write(register_t::MODEM_CONFIG_2, config[1]);
+  reg_write(register_t::MODEM_CONFIG_3, config[2]);
+}
+
+
+void RF95::preamble_length(uint16_t bytes)
+{
+  reg_write(register_t::PREAMBLE_MSB, uint8_t(bytes >> 8));
+  reg_write(register_t::PREAMBLE_LSB, uint8_t(bytes));
+}
+
+
+void RF95::frequency(float centre)
+{
+  uint32_t frf = (centre * 1000000.0) / RH_RF95_FSTEP;
+  reg_write(register_t::FREQ_MSB, uint8_t(frf >> 16));
+  reg_write(register_t::FREQ_MID, uint8_t(frf >> 8));
+  reg_write(register_t::FREQ_LSB, uint8_t(frf));
+}
+
+
+void RF95::tx_power(int power)
+{
+  // Apparently my module only uses the high power
+  // output.
+  assert(power >= 2 && power <= 20);
+  if(power > 17)
+  {
+    // According to RadioHead, the DAC enabling gives
+    // us around 3 DB.
+    reg_write(register_t::PA_DAC, RH_RF95_PA_DAC_ENABLE);
+    power -= 3;
+  }
+  else
+  {
+    reg_write(register_t::PA_DAC, RH_RF95_PA_DAC_DISABLE);
+  }
+  reg_write(register_t::PA_CONFIG, RH_RF95_PA_SELECT | (power-2));
+}
+
+
+void RF95::mode(mode_t mode)
+{
+  // I'm a bit suprised this works, because
+  // I would have thought overwriting the LoRa
+  // bit like this is a bad idea. I presume it works
+  // as long as we don't sleep?
+  switch(mode)
+  {
+  case IDLE:
+    reg_write(register_t::OP_MODE, uint8_t(op_reg::STDBY));
+    break;
+  case TX:
+    reg_write(register_t::OP_MODE, uint8_t(op_reg::TX));
+    break;
+  }
+}
+
+
+uint8_t RF95::reg_read(register_t register_)
 {
   esp_err_t res;
   uint8_t ret;
   struct spi_transaction_t t;
   std::memset(&t, 0, sizeof(t));
   t.length = 16;
-  t.tx_data[0] = register_ & 0x7f; // force read
+  t.tx_data[0] = uint8_t(register_) & 0x7f; // force read
   t.tx_data[1] = 0;
   t.flags = SPI_TRANS_USE_TXDATA | SPI_TRANS_USE_RXDATA;
   res = spi_device_transmit(_spi, &t);
@@ -88,14 +226,13 @@ uint8_t RF95::reg_read(uint8_t register_)
   return ret;
 }
 
-void RF95::reg_write(uint8_t register_, uint8_t value)
+void RF95::reg_write(register_t register_, uint8_t value)
 {
   esp_err_t res;
-  uint8_t ret;
   struct spi_transaction_t t;
   std::memset(&t, 0, sizeof(t));
   t.length = 16;
-  t.tx_data[0] = (register_ & 0x7f) | 0x80; // force write
+  t.tx_data[0] = (uint8_t(register_) & 0x7f) | 0x80; // force write
   t.tx_data[1] = value;
   t.flags = SPI_TRANS_USE_TXDATA | SPI_TRANS_USE_RXDATA;
   res = spi_device_transmit(_spi, &t);
